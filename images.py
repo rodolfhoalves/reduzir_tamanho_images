a35bdb598cf7feb241b374ad3c66fb1f915d4850
Rodolfho Alves <36309850+rodolfhoalves@users.noreply.github.com>
2020-02-23 16:56:41 -0300

    Create images.py

---
+++ images.py
@@ -0,0 +1,19 @@
+import os
+from PIL import Image
+from os import listdir
+
+source_images_path = "/home/rodolfho/Pictures/images-para-reduzir/"
+destination_images_path = "/home/rodolfho/Pictures/images-para-reduzir/novas_images/"
+
+source_images_list  = listdir(source_images_path)
+
+
+for file in source_images_list:         
+        image = f"{source_images_path}{file}"
+        if os.path.isfile(image):
+           
+            image = Image.open(image)
+            image.save(f"{destination_images_path}{file}", quality=20, optimize=True)
+
+        else:
+            continue
